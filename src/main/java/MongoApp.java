a0a045ccc83de4272b897ce33d1e9ccf93689acb
Egor18032019 <goro4@mail.ru>
2021-09-18 18:14:51 +0500

    добавил чтение из properies файла

--- src/main/java/MongoApp.java
+++ src/main/java/MongoApp.java
@@ -2,23 +2,58 @@ import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
 import org.bson.Document;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Collections;
+import java.util.Properties;
 import java.util.TreeMap;
 
 public class MongoApp {
-    private static final String MONGO_URL = "mongodb+srv://goro4:Qw222324@cluster0.j220x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    private static final String MONGO_URL = getKeyFromFile();
 
     public static void main(TreeMap<String, Integer> args) {
-
         MongoClient client = new MongoClient(new MongoClientURI(MONGO_URL));
         MongoDatabase database = client.getDatabase("simbirSoft");
         MongoCollection<Document> collection = database.getCollection("static");
 
         System.out.println("connected db");
-        Document stat = new Document("stat",Collections.unmodifiableSortedMap(args));
-        collection.insertOne(stat);
+        Document stat = new Document("name", "statistics");
+        stat.put("stat", Collections.unmodifiableSortedMap(args));
+        Document isFounded = collection.find(Filters.eq("name", "statistics")).first();
+        if (isFounded == null) {
+            collection.insertOne(stat);
+        } else {
+            collection.updateOne(Filters.eq("name", "statistics"), new Document("$set", Collections.unmodifiableSortedMap(args)));
+        }
         System.out.println("send in db");
     }
+
+    /**
+     * Получение ключ для монго из properti файла
+     *
+     * @return строку с данными
+     */
+    public static String getKeyFromFile() {
+        FileInputStream fis = null;
+        Properties property = new Properties();
+        String path = "src/main/resources/config.properties";
+        try {
+            fis = new FileInputStream(path);
+            property.load(fis);
+
+            String key = property.getProperty("mongoDB");
+
+            System.out.println("Key find ! ");
+            fis.close();
+            return key;
+        } catch (IOException e) {
+            System.err.println("Error: no can find properties file!");
+        }
+
+        return null;
+    }
 }
+
